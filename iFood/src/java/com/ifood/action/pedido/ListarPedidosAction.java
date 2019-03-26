b938ec06db2db3544ac7cdb4b7ef23aa8a9772ae
Jonathas Araújo <jonathas.araujo@engenharia.ufjf.br>
2019-03-26 15:33:36 -0300

    update

--- iFood/src/java/com/ifood/action/pedido/ListarPedidosAction.java
+++ iFood/src/java/com/ifood/action/pedido/ListarPedidosAction.java
@@ -18,22 +18,11 @@ import javax.servlet.ServletException;
  * @author jonat
  */
 public class ListarPedidosAction implements Action {
-    
-    private Object view;
 
-    public void execute(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
-        try {
-            request.setAttribute("Pedidos", PedidoDAO.getInstance().list());
-            RequestDispatcher view
-                    = request.getRequestDispatcher("/produtos.jsp");
-            try {
-                view.forward(request, response);
-            } catch (ServletException ex) {
-                Logger.getLogger(ListarPedidosAction.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        } catch (ClassNotFoundException ex) {
-        }
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
+   
 }
