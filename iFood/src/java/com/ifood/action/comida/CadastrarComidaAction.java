2ad6a6893beec79767e660279b13f27c60eab339
Flávio Henrique de Almeida Junior <flaviohenriquejf@gmail.com>
2019-03-28 14:16:39 -0300

    Inserção do Comida e administrador Action

--- iFood/src/java/com/ifood/action/comida/CadastrarComidaAction.java
+++ iFood/src/java/com/ifood/action/comida/CadastrarComidaAction.java
@@ -5,7 +5,56 @@
  */
 package com.ifood.action.comida;
 
+import com.ifood.controller.Action;
+import com.ifood.model.Comida;
+import com.ifood.model.Restaurante;
+import com.ifood.persistence.ComidaDAO;
+import com.ifood.persistence.RestauranteDAO;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class CadastrarComidaAction {
+
+
+public class CadastrarComidaAction implements Action{
     
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String nome = request.getParameter("textNome");
+        String descricao = request.getParameter("textDescricao");
+        String preco = request.getParameter("textPreco");
+        double precoComida = Double.parseDouble(preco);
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        Restaurante restaurante;
+        try {
+            restaurante = RestauranteDAO.getInstance().get(id);
+            Comida comida = new Comida(nome, descricao, precoComida, restaurante);                
+            ComidaDAO.getInstance().save(comida);
+        
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(CadastrarComidaAction.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+         catch (SQLException ex) {
+            Logger.getLogger(CadastrarComidaAction.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            request.getRequestDispatcher("index.jsp").include(request, response);
+        } catch (ServletException ex) {
+            Logger.getLogger(CadastrarComidaAction.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
 }
+
+
+
+
+
+    
